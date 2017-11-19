1d6672606744584246245248cc890b1732edd0d3
lapots <sebastrident@gmail.com>
2017-11-19 17:46:21 +0300

    Template for application

---
+++ src/main/java/com/lapots/game/journey/config/GrandJourneyConfigApplication.java
@@ -0,0 +1,13 @@
+package com.lapots.game.journey.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandJourneyConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrandJourneyConfigApplication.class, args);
+    }
+
+}
---
+++ start.bat
@@ -0,0 +1,1 @@
+mvnw clean install && java -jar target/grand-journey-config-server-0.0.1-SNAPSHOT.jar
